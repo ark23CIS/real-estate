7ec0d8d6333a35c5991ea50d01061af2909336cb
Danila Stryzhonok <36641744+ark23CIS@users.noreply.github.com>
2020-10-05 11:30:18 +0300

    profile info change

--- src/components/pages/Profile/index.js
+++ src/components/pages/Profile/index.js
@@ -77,21 +77,39 @@ function Profile({ match, history }) {
         </Banner>
       )}
       {currentProfile && (
-        <div>
-          <div>{currentProfile.isOnline ? 'Online' : 'Offline'}</div>
-          <div>{`${currentProfile.user.firstName} ${currentProfile.user.lastName}`}</div>
-          <Avatar src={currentProfile.photo === 'default' ? '' : currentProfile.photo} />
-          {isOwnPage && <Delete className="cursor" onClick={onDeleteProfile} />}
-          {currentProfile.social &&
-            Object.keys(currentProfile.social).map((key, index) => (
-              <SocialIcon
-                url={currentProfile.social[key]}
-                key={`${currentProfile.social[key]}_${index}`}
-              />
-            ))}
-          {currentProfile.contactNumber && (
-            <div>Contact Number: {currentProfile.contactNumber}</div>
-          )}
+        <div className="profile">
+          <div className="profile__main-info">
+            <div className="profile__status">{currentProfile.isOnline ? 'Online' : 'Offline'}</div>
+
+            <Avatar
+              src={currentProfile.photo === 'default' ? '' : currentProfile.photo}
+              style={{ height: '200px', width: '200px' }}
+            />
+            <div className="profile__fullname-and-delete">
+              {isOwnPage && <Delete className="cursor" onClick={onDeleteProfile} />}
+
+              <div className="profile__fullname">{`${currentProfile.user.firstName} ${currentProfile.user.lastName}`}</div>
+            </div>
+            <div className="profile__joined">
+              {`Joined ${new Date(currentProfile.user.dateOfRegistration).toDateString()}`}
+            </div>
+          </div>
+          <div className="profile__info">
+            {currentProfile.social && (
+              <div className="profile__socials">
+                {Object.keys(currentProfile.social).map((key, index) => (
+                  <SocialIcon
+                    url={currentProfile.social[key]}
+                    key={`${currentProfile.social[key]}_${index}`}
+                  />
+                ))}
+              </div>
+            )}
+            {currentProfile.contactNumber && (
+              <div>Contact Number : {currentProfile.contactNumber}</div>
+            )}
+            <div>Email : {currentProfile.user.email}</div>
+          </div>
           <TabComponent
             tabItems={
               user
@@ -107,34 +125,37 @@ function Profile({ match, history }) {
                   )
             }
           />
-          <Rating
-            authUserID={user ? user._id : ''}
-            collectionID={match.params.profileID}
-            label="profile"
-            ratingValue={currentProfile.totalRating}
-            isClickable={!!profile}
-            pageType="single"
-          />
-          <Like
-            likeType="profile"
-            pageType="single"
-            collectionID={
-              user ? (match.params.profileID === 'me' ? user._id : match.params.profileID) : ''
-            }
-            amountOflikes={currentProfile.amountOflikes}
-            isActive={user ? currentProfile.likes.includes(user._id) : false}
-            isClickable={!!profile}
-          />
-          <Dislike
-            dislikeType="profile"
-            pageType="single"
-            collectionID={
-              user ? (match.params.profileID === 'me' ? user._id : match.params.profileID) : ''
-            }
-            amountOfDislikes={currentProfile.amountOfdislikes}
-            isActive={user ? currentProfile.dislikes.includes(user._id) : false}
-            isClickable={!!profile}
-          />
+          <div className="profile__estimations">
+            <Rating
+              authUserID={user ? user._id : ''}
+              collectionID={match.params.profileID}
+              label="profile"
+              ratingValue={currentProfile.totalRating}
+              isClickable={!!profile}
+              pageType="single"
+            />
+            <Like
+              likeType="profile"
+              pageType="single"
+              collectionID={
+                user ? (match.params.profileID === 'me' ? user._id : match.params.profileID) : ''
+              }
+              amountOflikes={currentProfile.amountOflikes}
+              isActive={user ? currentProfile.likes.includes(user._id) : false}
+              isClickable={!!profile}
+            />
+            <Dislike
+              dislikeType="profile"
+              pageType="single"
+              collectionID={
+                user ? (match.params.profileID === 'me' ? user._id : match.params.profileID) : ''
+              }
+              amountOfDislikes={currentProfile.amountOfdislikes}
+              isActive={user ? currentProfile.dislikes.includes(user._id) : false}
+              isClickable={!!profile}
+            />
+          </div>
+
           <Comments
             comments={currentProfile.comments}
             label="profile"
