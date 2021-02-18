7ab1069df5c0632c404ae3d56b0fad6a8f19bdc8
Danila Stryzhonok <36641744+ark23CIS@users.noreply.github.com>
2021-02-18 12:06:01 +0300

    webpack fixes due to linter

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,108 +1,104 @@
 const path = require('path');
 const webpack = require('webpack');
-const htmlWebpackPlugin = require('html-webpack-plugin');
-const minifyPlugin = require('babel-minify-webpack-plugin');
-const optimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const miniCSSExtractPlugin = require('mini-css-extract-plugin');
-const compressionPlugin = require('compression-webpack-plugin');
-const brotliPlugin = require('brotli-webpack-plugin');
-const terserPlugin = require('terser-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MinifyPlugin = require('babel-minify-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
-module.exports = (env, options) => {
-  return {
-    entry: [
-      'react-hot-loader/patch',
-      'babel-runtime/regenerator',
-      'webpack-hot-middleware/client?reload=true',
-      './src/index.js',
-    ],
-    output: {
-      path: path.resolve(__dirname, '../build'),
-      filename: '[name]-bundle.js',
-    },
-    mode: 'development',
-    devtool: 'eval-source-map',
-    devServer: {
-      historyApiFallback: true,
-      contentBase: 'build',
-      overlay: true,
-      hot: true,
-      open: true,
-    },
-    optimization: {
-      nodeEnv: 'production',
-      minimize: false,
-      minimizer: [new terserPlugin()],
-      noEmitOnErrors: true,
-      removeEmptyChunks: true,
-      mergeDuplicateChunks: true,
-      removeAvailableModules: true,
-      // most usable modules will find earlier
-      occurrenceOrder: true,
-      concatenateModules: true,
-      providedExports: true, // defines exported entities for each module
-      usedExports: true,
-      sideEffects: true, // (TREE shaking) depends on providedExport & usedExports
-    },
-    module: {
-      rules: [
-        {
-          loader: 'html-loader',
-          test: /\.html$/,
-        },
-        {
-          loader: 'babel-loader',
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-        },
-        // {
-        //   test: /\.js$/,
-        //   exclude: /node_modules/,
-        //   loader: 'eslint-loader',
-        //   enforce: 'pre',
-        // },
-        {
-          test: /\.(css|scss|sass)$/,
-          exclude: /\.module\.(css|sass|scss)$/,
-          use: [
-            miniCSSExtractPlugin.loader,
-            {
-              loader: 'css-loader',
-              options: {
-                importLoaders: 1,
-              },
+module.exports = () => ({
+  entry: [
+    'react-hot-loader/patch',
+    'babel-runtime/regenerator',
+    'webpack-hot-middleware/client?reload=true',
+    './src/index.js',
+  ],
+  output: {
+    path: path.resolve(__dirname, '../build'),
+    filename: '[name]-bundle.js',
+  },
+  mode: 'development',
+  devtool: 'eval-source-map',
+  devServer: {
+    historyApiFallback: true,
+    contentBase: 'build',
+    overlay: true,
+    hot: true,
+    open: true,
+  },
+  optimization: {
+    nodeEnv: 'production',
+    minimize: false,
+    minimizer: [new TerserPlugin()],
+    noEmitOnErrors: true,
+    removeEmptyChunks: true,
+    mergeDuplicateChunks: true,
+    removeAvailableModules: true,
+    // most usable modules will find earlier
+    occurrenceOrder: true,
+    concatenateModules: true,
+    providedExports: true, // defines exported entities for each module
+    usedExports: true,
+    sideEffects: true, // (TREE shaking) depends on providedExport & usedExports
+  },
+  module: {
+    rules: [
+      {
+        loader: 'html-loader',
+        test: /\.html$/,
+      },
+      {
+        loader: 'babel-loader',
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+      },
+      // {
+      //   test: /\.js$/,
+      //   exclude: /node_modules/,
+      //   loader: 'eslint-loader',
+      //   enforce: 'pre',
+      // },
+      {
+        test: /\.(css|scss|sass)$/,
+        exclude: /\.module\.(css|sass|scss)$/,
+        use: [
+          MiniCSSExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
             },
-            'sass-loader',
-          ],
-        },
-        {
-          loader: 'file-loader',
-          test: /\.(woff|woff2|eot|ttf|otf)$/,
-        },
-        {
-          test: /\.(png|svg|jpg|gif)$/,
-          use: [
-            {
-              loader: 'url-loader',
-              options: {
-                limit: 8192,
-              },
+          },
+          'sass-loader',
+        ],
+      },
+      {
+        loader: 'file-loader',
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
             },
-          ],
-        },
-      ],
-    },
-    plugins: [
-      new optimizeCSSAssetsPlugin(),
-      new webpack.HotModuleReplacementPlugin(),
-      new htmlWebpackPlugin({
-        template: path.resolve(__dirname, '../public/index.html'),
-        title: 'Real Estate',
-      }),
-      new miniCSSExtractPlugin({
-        filename: '[name]-[contenthash].css',
-      }),
-      new minifyPlugin(),
+          },
+        ],
+      },
     ],
-  };
-};
+  },
+  plugins: [
+    new OptimizeCSSAssetsPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '../public/index.html'),
+      title: 'Real Estate',
+    }),
+    new MiniCSSExtractPlugin({
+      filename: '[name]-[contenthash].css',
+    }),
+    new MinifyPlugin(),
+  ],
+});
